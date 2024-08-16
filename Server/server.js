95045af523cad151af0762f5ae7acccfd9b60230
Kishore G <85576769+kishore-freak653@users.noreply.github.com>
2024-08-16 23:15:43 +0530

    Update server.js
    Server.js Updated

--- Server/server.js
+++ Server/server.js
@@ -1,46 +1,46 @@
-// import express from "express";
-// import cors from "cors";
-// import connectDB from "./config/db.js";
-// import authRoutes from "./routes/Blog.js"
-// import cookieParser from "cookie-parser";
-// const app = express();
-// const PORT = 8000;
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/Blog.js"
+import cookieParser from "cookie-parser";
+const app = express();
+const PORT = 8000;
 
 
-// //DataBase Connection
-// connectDB();
-// app.get('/',(req,res)=>{
-//   res.send("API is Running...")
-// })
+//DataBase Connection
+connectDB();
+app.get('/',(req,res)=>{
+  res.send("API is Running...")
+})
 
-// // Middleware for parsing cookies
-// app.use(cookieParser()); // Add this line to parse cookies
+// Middleware for parsing cookies
+app.use(cookieParser()); // Add this line to parse cookies
 
-// //CORS 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173"],
-//     methods: ["POST", "GET", "PUT", "DELETE"],
-//     credentials: true, // Allow credentials (cookies, authorization headers)
-//   })
-// );
+//CORS 
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  })
+);
 
-// //Json parse MiddleWare 
-// app.use(express.json());
+//Json parse MiddleWare 
+app.use(express.json());
 
-// app.use(express.static("public/upload"));
+app.use(express.static("public/upload"));
 
-// //api routes
-// app.use("/api/auth",authRoutes);
+//api routes
+app.use("/api/auth",authRoutes);
 
 
 
-// //Server Connection
-// app.listen(PORT,(err)=>{
-//   if(err){
-//     console.log("Error Connecting Server");
-//   }
-//   else{
-//     console.log("Server Connected at PORT :",+PORT)
-//   }
-// })
+//Server Connection
+app.listen(PORT,(err)=>{
+  if(err){
+    console.log("Error Connecting Server");
+  }
+  else{
+    console.log("Server Connected at PORT :",+PORT)
+  }
+})
